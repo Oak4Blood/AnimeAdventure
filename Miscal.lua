a544239fad204ff64af3425af9a30d611c812962
RedOak <97004533+Oak4Blood@users.noreply.github.com>
2025-01-18 02:39:41 +0700

    Update Miscal.lua

--- Miscal.lua
+++ Miscal.lua
@@ -31,9 +31,67 @@ local p = game:GetService("Players").LocalPlayer
 ts:Teleport(game.PlaceId, p)
 end
 
+
+local function toggleui()
+    local gmaae = workspace
+
+for i, v in pairs(gmaae:GetChildren()) do
+    if v.Name == "weather" then
+
+local plyui = game:GetService("Players").LocalPlayer.PlayerGui
+
+repeat
+    wait()
+until plyui:FindFirstChild("IsFarming") and plyui:FindFirstChild("Unit_ID_List")
+
+local Farmgui = game:GetService("Players").LocalPlayer.PlayerGui.IsFarming
+local UnitList = game:GetService("Players").LocalPlayer.PlayerGui.Unit_ID_List
+
+local ScreenGui = Instance.new("ScreenGui")
+ScreenGui.Name = "ToggleFarm"
+ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+
+local Frame = Instance.new("Frame")
+Frame.Size = UDim2.new(0, 45, 0, 45)
+Frame.Position = UDim2.new(0.768, 0, -0.03, 0)
+Frame.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+Frame.BorderSizePixel = 0
+Frame.Active = true
+Frame.Draggable = true
+Frame.Parent = ScreenGui
+
+local Button = Instance.new("TextButton")
+Button.Size = UDim2.new(1, 0, 1, 0)
+Button.BackgroundColor3 = Color3.fromRGB(100, 100, 100)
+Button.Text = "Hide"
+Button.TextColor3 = Color3.fromRGB(255, 255, 255)
+Button.Font = Enum.Font.SourceSans
+Button.TextSize = 15
+Button.Parent = Frame
+
+local isEnabled = false
+
+Button.MouseButton1Click:Connect(function()
+    isEnabled = not isEnabled
+    if isEnabled then
+        Button.Text = "Hide"
+        Button.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+        Farmgui.Enabled = true
+        UnitList.Enabled = true
+    else
+        Button.Text = "Show"
+        Button.BackgroundColor3 = Color3.fromRGB(0, 255, 0)
+        Farmgui.Enabled = false
+        UnitList.Enabled = false
+    end
+end)
+end
+end
+end
 -- Call Function
 showgem()
 message()
+toggleui()
 
 -- Auto Vote Start
 local earth1 = workspace
