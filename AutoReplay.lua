ceda982f244910fe8a0745100750b9937375439e
RedOak <97004533+Oak4Blood@users.noreply.github.com>
2025-01-25 02:00:59 +0700

    Update AutoReplay.lua

--- AutoReplay.lua
+++ AutoReplay.lua
@@ -1,6 +1,47 @@
 _G.autoreplay = true
 print("AutoReplay Executed")
 
+local function replaytease()
+    local function SendMessageEMBED(url, embed)
+        local headers = { ["Content-Type"] = "application/json" }
+        local data = {
+            ["username"] = "👩‍💻 Your Private Assistant",
+            ["avatar_url"] = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/caf1f59c-cbae-4e80-adf0-0717491aaad7/dga5nnp-471e7f06-245b-413c-803b-bbddb3d85090.jpg/v1/fill/w_768,h_1024,q_75,strp/sexy_secretary_b_8301f_by_incidesign_dga5nnp-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTAyNCIsInBhdGgiOiJcL2ZcL2NhZjFmNTljLWNiYWUtNGU4MC1hZGYwLTA3MTc0OTFhYWFkN1wvZGdhNW5ucC00NzFlN2YwNi0yNDViLTQxM2MtODAzYi1iYmRkYjNkODUwOTAuanBnIiwid2lkdGgiOiI8PTc2OCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.2k68_eMAZZ5DnzQB-kBzCLSuJWK5i6lhAqei_AwAdPE",
+            ["embeds"] = {
+                {
+                    ["title"] = embed.title,
+                    ["color"] = embed.color,
+                    ["fields"] = embed.fields,
+                    ["footer"] = { ["text"] = embed.footer.text },
+                    ["timestamp"] = os.date("!%Y-%m-%dT%H:%M:%SZ")
+                }
+            }
+        }
+        
+        local httpService = game:GetService("HttpService")
+        local body = httpService:JSONEncode(data)
+        http_request({ Url = url, Method = "POST", Headers = headers, Body = body })
+    end
+
+    local function randomHexColor()
+        return math.random(0, 0xFFFFFF)
+    end
+    local color = randomHexColor()
+    local function getFormattedDateTime()
+        return os.date("%Y-%m-%d %H:%M:%S")
+    end
+
+    local embed = {
+        ["title"] = "รายงานค่ะท่าน!",
+        ["color"] = color,
+        ["fields"] = {
+            { ["name"] = "รีเพลย์ Replay ให้คุณ "..game.Players.LocalPlayer.Character.Name, ["value"] = "เรียบร้อยค่ะ Success!" }
+        },
+        ["footer"] = { ["text"] = getFormattedDateTime() }
+    }
+    SendMessageEMBED(_G.webhook_url, embed)
+end
+
 while _G.autoreplay do
 
 wait(1)
@@ -35,6 +76,7 @@ local args = {
 }
 
 game:GetService("ReplicatedStorage"):WaitForChild("endpoints"):WaitForChild("client_to_server"):WaitForChild("set_game_finished_vote"):InvokeServer(unpack(args))
+replaytease()
 
 _G.autoreplay = false
 end
